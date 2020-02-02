7beb188b966fdb0de347b2feef1193fe42778b42
feihu1996x <gsx0312@qq.com>
2020-02-03 00:55:49 +0800

    自己编写Koa路由中间件

--- index.js
+++ index.js
@@ -2,20 +2,24 @@ const Koa = require('koa');
 
 const app = new Koa();
 
-// 1 3 5 4 2
-app.use(async (ctx, next) => {
-    console.log(1);
-    await next();
-    console.log(2);
-    ctx.body = 'Hello, Zhihu API';
-});
-app.use(async (ctx, next) => {
-    console.log(3);
-    await next();
-    console.log(4);
-});
 app.use(async (ctx) => {
-    console.log(5);
+    if ('/' === ctx.url) {
+        ctx.body = '这是主页';
+    } else if ('/users' === ctx.url) {
+        if ('GET' === ctx.method) {
+            ctx.body = '这是用户列表页';
+        } else if ('POST' == ctx.method) {
+            ctx.body = '创建用户';
+        } else {
+            ctx.status = 405;
+        }
+    } else if (ctx.url.match(/\/users\/\w+/)) {
+        const userId = ctx.url.match(/\/users\/(\w+)/)[1];
+        ctx.body = `这是用户${userId}`;
+    } else {
+        ctx.status = 404;
+    }
 });
+
 
 app.listen(3000);
