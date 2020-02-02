53fe0b0dcb613b3107258fe7c70c03304cc7874e
feihu1996x <gsx0312@qq.com>
2020-02-02 22:36:11 +0800

    Koa中间件与洋葱模型

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const Koa = require('koa');
 
 const app = new Koa();
 
-
-app.use((ctx) => {
+// 1 3 5 4 2
+app.use(async (ctx, next) => {
+    console.log(1);
+    await next();
+    console.log(2);
     ctx.body = 'Hello, Zhihu API';
+});
+app.use(async (ctx, next) => {
+    console.log(3);
+    await next();
+    console.log(4);
+});
+app.use(async (ctx) => {
+    console.log(5);
 });
 
 app.listen(3000);
